03f675d92544d8865e1bc5609846eec4d0dfec80
tobibot <tobias.heying@outlook.com>
2022-05-26 12:28:03 +0200

    isbn10

--- isbn10validation/kata.go
+++ isbn10validation/kata.go
@@ -1,7 +1,30 @@
 package isbn10validation
 
+import (
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var (
+	digitRegexp = regexp.MustCompile(`\d`)
+)
 
 func ValidISBN10(isbn string) bool {
-	return false // implement proper solution here
-  }
-  
+	if len(isbn) != 10 {
+		return false
+	}
+	s := strings.Split(isbn, "")
+	sum := 0
+	for i, r := range s {		
+		if digitRegexp.MatchString(r) {
+			toAdd, _ := strconv.Atoi(r)
+			sum += toAdd * (i + 1)			
+		} else if i== 9 && (r == "X" || r == "x") {
+			sum += (i + 1) * 10
+		} else {
+			return false
+		}
+	}
+	return sum%11 == 0
+}
--- isbn10validation/kata_test.go
+++ isbn10validation/kata_test.go
@@ -1,6 +1,8 @@
 package isbn10validation
 
 import (
+	"testing"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -21,3 +23,31 @@ var _ = Describe("ISBN Validator", func() {
 		Expect(ValidISBN10("XXXXXXXXXX")).To(Equal(false), "XXXXXXXXXX is not valid")
 	})
 })
+
+func TestValidISBN10(t *testing.T) {
+	tests := []struct {
+		name string
+		isbn string
+		want bool
+	}{
+		// {"1112223339 is valid", "1112223339", true},
+		// {"048665088X is valid", "048665088X", true},
+		// {"1293000000 is valid", "1293000000", true},
+		// {"1234554321 is valid", "1234554321", true},
+		{"X123456788 is valid", "048665088x", true},
+
+		// {"1234512345 is not valid", "1234512345", false},
+		// {"1293 is not valid", "1293", false},
+		// {"X123456788 is not valid", "X123456788", false},
+
+		// {"ABCDEFGHIJ is not valid", "ABCDEFGHIJ", false},
+		// {"XXXXXXXXXX is not valid", "XXXXXXXXXX", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ValidISBN10(tt.isbn); got != tt.want {
+				t.Errorf("ValidISBN10() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
