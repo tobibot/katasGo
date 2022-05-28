454d5cc0d26d617fd3fc401c7ea9d0d4ef3fbeb4
tobibot <tobias.heying@outlook.com>
2022-05-28 22:17:47 +0200

    next kata. UCL final boring

---
+++ stringsMix/kata.go
@@ -0,0 +1,6 @@
+package kata
+
+func Mix(s1, s2 string) string {
+	// your code
+	return ""
+}
---
+++ stringsMix/kata_test.go
@@ -0,0 +1,32 @@
+package kata_test
+
+import (
+	"testing"
+
+	kata "github.com/tobibot/katasGo/stringsMix"
+)
+
+func TestMix(t *testing.T) {
+	tests := []struct {
+		name string
+		s1 string
+		s2 string
+		want string
+	}{
+		{"Are they here", "yes, they are here", "2:eeeee/2:yy/=:hh/=:rr"},
+		{"uuuuuu", "uuuuuu", "=:uuuuuu"},
+		{"looping is fun but dangerous", "less dangerous than coding", "1:ooo/1:uuu/2:sss/=:nnn/1:ii/2:aa/2:dd/2:ee/=:gg"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := kata.Mix(tt.s1, tt.s2); got != tt.want {
+				t.Errorf("Mix() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+dotest("2:eeeee/2:yy/=:hh/=:rr")
+dotest("uuuuuu", "uuuuuu", "=:uuuuuu")
+dotest("looping is fun but dangerous", "less dangerous than coding", 
+		"1:ooo/1:uuu/2:sss/=:nnn/1:ii/2:aa/2:dd/2:ee/=:gg")
