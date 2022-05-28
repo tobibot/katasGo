18601ead562ae9502271bdd2ce24357d186fb72c
tobibot <tobias.heying@outlook.com>
2022-05-28 22:34:03 +0200

    test cases

--- stringsMix/kata_test.go
+++ stringsMix/kata_test.go
@@ -13,9 +13,9 @@ func TestMix(t *testing.T) {
 		s2 string
 		want string
 	}{
-		{"Are they here", "yes, they are here", "2:eeeee/2:yy/=:hh/=:rr"},
-		{"uuuuuu", "uuuuuu", "=:uuuuuu"},
-		{"looping is fun but dangerous", "less dangerous than coding", "1:ooo/1:uuu/2:sss/=:nnn/1:ii/2:aa/2:dd/2:ee/=:gg"},
+		{"here?", "Are they here", "yes, they are here", "2:eeeee/2:yy/=:hh/=:rr"},
+		{"some u's", "uuuuuu", "uuuuuu", "=:uuuuuu"},
+		{"looping", "looping is fun but dangerous", "less dangerous than coding", "1:ooo/1:uuu/2:sss/=:nnn/1:ii/2:aa/2:dd/2:ee/=:gg"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
